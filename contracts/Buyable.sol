366fcdb5e6e3e4835244dadfd57a606d842fd61a
UnivDev <univdev99@gmail.com>
2021-06-03 03:28:46 -0400

    1155 implementation

--- contracts/Buyable.sol
+++ contracts/Buyable.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
-import "openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
 
-contract Buyable is Ownable, Pausable, Destructible {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract Buyable is Ownable, Pausable {
     event Sent(address indexed payee, uint256 amount, uint256 balance);
     event Received(
         address indexed payer,
@@ -16,7 +17,7 @@ contract Buyable is Ownable, Pausable, Destructible {
     ERC721 public nftAddress;
     uint256 public currentPrice;
 
-    constructor(address _nftAddress, uint256 _currentPrice) public {
+    constructor(address _nftAddress, uint256 _currentPrice) {
         require(_nftAddress != address(0) && _nftAddress != address(this));
         require(_currentPrice > 0);
         nftAddress = ERC721(_nftAddress);
--- test/sample-test.js
+++ test/sample-test.js
@@ -10,3 +10,24 @@ describe("SingleAsset", function () {
 		expect(await nft.tokenURI(1)).to.equal(URI);
 	});
 });
+
+describe("MultiAsset", function () {
+	it("It should deploy the contract, mint a token, and resolve to the right URI", async function () {
+		const NFT = await ethers.getContractFactory("MultiAsset");
+		const nft = await NFT.deploy();
+		const URI = "ipfs://QmWJBNeQAm9Rh4YaW8GFRnSgwa4dN889VKm9poc2DQPBkv";
+		await nft.deployed();
+		await nft.mint("0xbA842b7DA417Ba762D75e8F99e11c2980a8F8051", URI);
+		expect(await nft.tokenURI(1)).to.equal(URI);
+	});
+
+	it("It should deploy the contract, mint batch tokens, and resolve to the right URI", async function () {
+		const NFT = await ethers.getContractFactory("MultiAsset");
+		const nft = await NFT.deploy();
+		const URIs = new Array(5).fill("ipfs://QmWJBNeQAm9Rh4YaW8GFRnSgwa4dN889VKm9poc2DQPBkv");
+		await nft.deployed();
+		await nft.mintBatch("0xbA842b7DA417Ba762D75e8F99e11c2980a8F8051", URIs);
+		expect(await nft.tokenURI(2)).to.equal(URIs[2]);
+		expect(await nft.tokenURI(4)).to.equal(URIs[4]);
+	});
+});
