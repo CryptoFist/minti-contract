fcf8e08d484637bee9f69ed872bac722a596bbab
UnivDev <univdev99@gmail.com>
2021-06-11 18:45:51 -0400

    Update auction test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,38 @@
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
+require("@nomiclabs/hardhat-truffle5");
+// require("dotenv").config();
+
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+// module.exports = {
+// 	defaultNetwork: "matic",
+// 	networks: {
+// 		hardhat: {},
+// 		matic: {
+// 			url: "https://rpc-mumbai.maticvigil.com",
+// 			accounts: [PRIVATE_KEY],
+// 		},
+// 	},
+// 	solidity: {
+// 		version: "0.8.4",
+// 		settings: {
+// 			optimizer: {
+// 				enabled: true,
+// 				runs: 200,
+// 			},
+// 		},
+// 	},
+// 	paths: {
+// 		sources: "./contracts",
+// 		tests: "./test",
+// 		cache: "./cache",
+// 		artifacts: "./artifacts",
+// 	},
+// 	mocha: {
+// 		timeout: 20000,
+// 	},
+// };
+
 module.exports = {
-	defaultNetwork: "matic",
-	networks: {
-		hardhat: {},
-		matic: {
-			url: "https://rpc-mumbai.maticvigil.com",
-			accounts: [PRIVATE_KEY],
-		},
-	},
 	solidity: {
 		version: "0.8.4",
 		settings: {
@@ -20,13 +42,4 @@ module.exports = {
 			},
 		},
 	},
-	paths: {
-		sources: "./contracts",
-		tests: "./test",
-		cache: "./cache",
-		artifacts: "./artifacts",
-	},
-	mocha: {
-		timeout: 20000,
-	},
-};
+  };
